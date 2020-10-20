5142a46ba0f0f98705d5e20691193a98b88daf9f
Erkki Tuomi <erkki.tuomi@lmsomeco.fi>
2020-10-20 09:48:16 +0300

    Missing language-link from header

--- partials/header.php
+++ partials/header.php
@@ -6,12 +6,13 @@
     <ul class="navbar-nav ml-auto mr-2">
       <img src="images/dropdown-circle.svg" class="dropdown-circle" style="width: 100% \\9;" />
       <li class="nav-item dropdown">
-        <!-- <div class="nav-link dropdown-toggle" onclick="toggle()" href="#" id="navbarDropdown" role="button"> -->
         <div class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button">
           ENG </div>
         <div id="dropdown" class="dropdown-menu" aria-labelledby="navbarDropdown" style="display:none;opacity:0">
           <div class="dropdown-item" id="eng">ENG</div>
           <div class="dropdown-item" id="swe">SWE</div>
+          <div class="dropdown-item" id="fi">FI</div>
+          <div class="dropdown-item" id="dk">DK</div>
         </div>
       </li>
     </ul>
