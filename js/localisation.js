d03bbcc4ed7ac9720c891247bcac8704f65ea589
eerol <eero.louhenpera@lmand.fi>
2021-02-16 11:07:10 +0200

    new package

--- js/localisation.js
+++ js/localisation.js
@@ -88,111 +88,116 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-var languages = ['eng', 'swe', 'fi', 'dk'];
-var navHeader = document.getElementById('navbarDropdown');
-
-function init() {
-  var languageOptions = document.getElementsByClassName('dropdown-item'); //Interaction
-
-  for (var i = 0; i < languageOptions.length; i++) {
-    var langOption = languageOptions[i];
-    langOption.addEventListener('click', function () {
-      callJSON(this.id);
-    });
-  }
-
-  navHeader.addEventListener('click', function () {
-    var dropDownElem = document.getElementById("dropdown");
-
-    if (dropDownElem.style.display === "none") {
-      dropDownElem.style.display = "block";
-      dropDownElem.style.opacity = "1";
-    } else {
-      dropDownElem.style.display = "none";
-      dropDownElem.style.opacity = "0";
-    }
-  });
-  callJSON(urlParameters());
-}
-/**
- * @description call selected language JSON
- * @param {String} currLang <- selected language
- */
-
-
-function callJSON(currLang) {
-  navHeader.innerHTML = currLang.toString();
-  var url = "assets/languages/" + currLang + ".json";
-  var Http = new XMLHttpRequest();
-
-  Http.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      changeLanguage(JSON.parse(this.responseText));
-    }
-  };
-
-  Http.open("GET", url, true);
-  Http.send();
-}
-
-function changeLanguage(data) {
-  //content fields
-  var title = document.getElementById('title');
-  var ingressi = document.getElementById('ingressi');
-  var recycle1 = document.getElementById('recycle1');
-  var recycle2 = document.getElementById('recycle2');
-  var cotwo1 = document.getElementById('cotwo1');
-  var cotwo2 = document.getElementById('cotwo2');
-  var cotwo3 = document.getElementById('cotwo3');
-  var plantpack1 = document.getElementById('plantpack1');
-  var plantpack2 = document.getElementById('plantpack2');
-  var endtitle = document.getElementById('endtitle'); // address
-
-  var companyName = document.getElementById("comapany-name");
-  var companyPostalAddress = document.getElementById("company-postal-address");
-  var companyWtf = document.getElementById("company-wtf");
-  var companyCity = document.getElementById("company-city");
-  var companyPhone = document.getElementById("company-phone");
-  var companyRegistrationNumber = document.getElementById("company-registration-number");
-  title.innerHTML = data.title;
-  ingressi.innerHTML = data.ingressi;
-  recycle1.innerHTML = data.recycle1;
-  recycle2.innerHTML = data.recycle2;
-  cotwo1.innerHTML = data.cotwo1;
-  cotwo2.innerHTML = data.cotwo2;
-  cotwo3.innerHTML = data.cotwo3;
-  plantpack1.innerHTML = data.plantpack1;
-  plantpack2.innerHTML = data.plantpack2;
-  endtitle.innerHTML = data.endtitle; // 
-
-  companyName.innerHTML = data.companyName;
-  companyPostalAddress.innerHTML = data.companyPostalAddress;
-  companyWtf.innerHTML = data.companyWtf;
-  companyCity.innerHTML = data.companyCity;
-  companyPhone.innerHTML = data.companyPhone;
-  companyRegistrationNumber.innerHTML = data.companyRegistrationNumber;
-}
-
-function urlParameters() {
-  var queryString = window.location.search;
-  var urlParameters = new URLSearchParams(queryString);
+  var languages = ['eng', 'swe', 'fi', 'dk', 'nl'];
   var navHeader = document.getElementById('navbarDropdown');
-
-  if (urlParameters.has('lang')) {
-    navHeader.innerHTML = urlParameters.get('lang').toUpperCase();
-    return urlParameters.get('lang');
+  
+  function init() {
+    var languageOptions = document.getElementsByClassName('dropdown-item'); //Interaction
+  
+    for (var i = 0; i < languageOptions.length; i++) {
+      var langOption = languageOptions[i];
+      langOption.addEventListener('click', function () {
+        callJSON(this.id);
+        document.body.className = ''
+        document.body.classList.add(this.id)
+      });
+    }
+  
+    navHeader.addEventListener('click', function () {
+      var dropDownElem = document.getElementById("dropdown");
+  
+      if (dropDownElem.style.display === "none") {
+        dropDownElem.style.display = "block";
+        dropDownElem.style.opacity = "1";
+      } else {
+        dropDownElem.style.display = "none";
+        dropDownElem.style.opacity = "0";
+      }
+    });
+    callJSON(urlParameters());
   }
-
-  navHeader.innerHTML = 'ENG';
-  return 'eng';
-}
-/**
- * SHADOW DOM
- * create drop down
- */
-
-
-window.onload = init();
-
-/***/ })
-/******/ ]);
+  /**
+   * @description call selected language JSON
+   * @param {String} currLang <- selected language
+   */
+  
+  
+  function callJSON(currLang) {
+    navHeader.innerHTML = currLang.toString();
+    var url = "assets/languages/" + currLang + ".json";
+    var Http = new XMLHttpRequest();
+  
+    Http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        changeLanguage(JSON.parse(this.responseText));
+      }
+    };
+  
+    Http.open("GET", url, true);
+    Http.send();
+  }
+  
+  function changeLanguage(data) {
+    //content fields
+    var title = document.getElementById('title');
+    var ingressi = document.getElementById('ingressi');
+    var recycle1 = document.getElementById('recycle1');
+    var recycle2 = document.getElementById('recycle2');
+    var cotwo1 = document.getElementById('cotwo1');
+    var cotwo2 = document.getElementById('cotwo2');
+    var cotwo3 = document.getElementById('cotwo3');
+    var plantpack1 = document.getElementById('plantpack1');
+    var plantpack2 = document.getElementById('plantpack2');
+    var endtitle = document.getElementById('endtitle'); // address
+  
+    var companyName = document.getElementById("comapany-name");
+    var companyPostalAddress = document.getElementById("company-postal-address");
+    var companyWtf = document.getElementById("company-wtf");
+    var companyCity = document.getElementById("company-city");
+    var companyPhone = document.getElementById("company-phone");
+    var companyRegistrationNumber = document.getElementById("company-registration-number");
+    title.innerHTML = data.title;
+    ingressi.innerHTML = data.ingressi;
+    recycle1.innerHTML = data.recycle1;
+    recycle2.innerHTML = data.recycle2;
+    cotwo1.innerHTML = data.cotwo1;
+    cotwo2.innerHTML = data.cotwo2;
+    cotwo3.innerHTML = data.cotwo3;
+    plantpack1.innerHTML = data.plantpack1;
+    plantpack2.innerHTML = data.plantpack2;
+    endtitle.innerHTML = data.endtitle; // 
+  
+    companyName.innerHTML = data.companyName;
+    companyPostalAddress.innerHTML = data.companyPostalAddress;
+    companyWtf.innerHTML = data.companyWtf;
+    companyCity.innerHTML = data.companyCity;
+    companyPhone.innerHTML = data.companyPhone;
+    companyRegistrationNumber.innerHTML = data.companyRegistrationNumber;
+  }
+  
+  function urlParameters() {
+    var queryString = window.location.search;
+    var urlParameters = new URLSearchParams(queryString);
+    var navHeader = document.getElementById('navbarDropdown');
+  
+    if (urlParameters.has('lang')) {
+      navHeader.innerHTML = urlParameters.get('lang').toUpperCase();
+      document.body.className = ''
+      document.body.classList.add(urlParameters.get('lang'))
+      return urlParameters.get('lang');
+    }
+  
+    navHeader.innerHTML = 'ENG';
+    document.body.classList.add('eng')
+    return 'eng';
+  }
+  /**
+   * SHADOW DOM
+   * create drop down
+   */
+  
+  
+  window.onload = init();
+  
+  /***/ })
+  /******/ ]);
